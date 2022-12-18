0470a1f9504609ffdb3a4a8dad7078a11ff51bae
SaifullaJubair <saifullajubair6@gmail.com>
2022-12-18 18:37:15 +0600

    nishad code add

--- components/Home/LifeStyleNews/LifeStyleNews.js
+++ components/Home/LifeStyleNews/LifeStyleNews.js
@@ -1,8 +1,92 @@
+import Link from "next/link";
 
 const LifeStyleNews = () => {
     return (
-        <div>
-            Life Style News
+        <div className="my-10 font-poppins ">
+            <div className="container mx-auto px-4 ">
+                <h1 className="text-3xl font-bold">LifeStyle</h1>
+                <hr className="border-red-500 mt-1" />
+            </div>
+            <section className="py-6 dark:bg-gray-800 dark:text-gray-50">
+                <div className="container grid grid-cols-2 gap-4 px-4 mx-auto md:grid-cols-4">
+                    {/* <img
+            src="https://source.unsplash.com/random/301x301/"
+            alt=""
+            className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square"
+          /> */}
+
+                    <Link href='#' >
+                        <div class=" w-full rounded overflow-hidden shadow-lg">
+                            <img
+                                src="https://source.unsplash.com/random/200x200/?7"
+                                className="w-full"
+                                alt=""
+                            />
+                            <div class="py-3">
+                                <div class="font-bold text-xl mb-2">
+                                    New app "DuiLingo" helps students talk to professors without
+                                    offending them
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                    <div class=" w-full rounded overflow-hidden shadow-lg">
+                        <img
+                            src="https://source.unsplash.com/random/200x200/?7"
+                            className="w-full"
+                            alt=""
+                        />
+                        <div class="py-3">
+                            <div class="font-bold text-xl mb-2">
+                                8 essential work-from-home apps to make your life easier
+                            </div>
+                        </div>
+                    </div>
+                    <div class=" w-full rounded overflow-hidden shadow-lg">
+                        <img
+                            src="https://source.unsplash.com/random/200x200/?7"
+                            className="w-full"
+                            alt=""
+                        />
+                        <div class="py-3">
+                            <div class="font-bold text-xl mb-2">
+                                6 ways to be productive at work
+                            </div>
+                        </div>
+                    </div>
+                    <div class=" w-full rounded overflow-hidden shadow-lg">
+                        <img
+                            src="https://source.unsplash.com/random/200x200/?7"
+                            className="w-full"
+                            alt=""
+                        />
+                        <div class="py-3">
+                            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                        </div>
+                    </div>
+                    <Link href='#' className="col-span-2 row-span-2
+           md:col-start-1 md:row-start-1 shadow-md "  >
+                        <div class=" w-full rounded overflow-hidden ">
+                            <img
+                                src="https://source.unsplash.com/random/200x200/?7"
+                                className="w-full"
+                                alt=""
+                            />
+                            <div class="py-3">
+                                <div class="font-bold text-2xl mb-2">
+                                    Int'l Migrants Day: with dreams departed, they come home dead
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+                                    vitae aperiam cum cupiditate nesciunt nam, sapiente ratione
+                                    provident saepe non porro. Autem praesentium repellendus
+                                    repellat nobis quis suscipit consequatur nostrum!
+                                </p>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            </section>
         </div>
     );
 };
--- components/Shared/Navbar/Navbar.js
+++ components/Shared/Navbar/Navbar.js
@@ -1,11 +1,119 @@
-import React from 'react';
 
-const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    );
-};
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import { Container } from '@mui/system';
+import Link from 'next/link';
+
+const drawerWidth = 240;
+const navItems = ['Wolrd', 'Politics', 'Trending', 'Economy', 'Business', 'Politics', 'Sports', 'Education', 'Youth', 'Tech Startup', 'Feature'];
+
+function Navbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2, fontFamily: 'poppins' }}>
+        News71
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center', fontFamily: 'poppins' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav" sx={{
+        backgroundColor: '#fff',
+        boxShadow: 'none',
+
+      }} >
+        <Toolbar>
+          <IconButton
+            color="#000"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box sx={{ flexGrow: 1, display: { xs: '', sm: 'block', color: '#000' } }} >
+            <Link href='/' >
+              <img src="https://i.ibb.co/dL4rzf2/152-1527067-cnn-png.png" alt=""
+                style={{ width: '150px' }} />
+            </Link>
+          </Box>
+          {/* <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block', color: '#000' } }}
+          >
+            News71
+          </Typography> */}
+          <Container>
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              {navItems.map((item) => (
+                <Button key={item} sx={{ color: '#000', fontFamily: 'poppins' }}>
+                  {item}
+                </Button>
+              ))}
+            </Box>
+          </Container>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+
+    </Box>
+
+  );
+}
+
+
 
 export default Navbar;
