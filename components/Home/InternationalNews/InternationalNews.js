d52d3f75ecb03e8d9b3123a9583264b02dcb20c0
unknown <gulam.mostafa209@gmail.com>
2022-12-30 00:43:02 +0600

    dynamic router

--- components/Home/InternationalNews/InternationalNews.js
+++ components/Home/InternationalNews/InternationalNews.js
@@ -44,7 +44,7 @@ const InternationalNews = () => {
         }
     })
 
-    // console.log(internationals)
+    // console.log(newsnationals)
 
     return (
         <div>
@@ -54,15 +54,15 @@ const InternationalNews = () => {
                     <div className="shadow-md p-3">
                         <h2> <span className="font-semibold text-lg">{'Ukraine'}'s</span> top News</h2>
                         {
-                            ukraine.map(inter => <div className="my-3 w-full duration-300 hover:-translate-y-2">
+                            ukraine.map(news => <div className="my-3 w-full duration-300 hover:-translate-y-2">
+                                <Link href={`/news/view/${news?._id}`} className=''>
+                                    <div className="mt-3 ">
+                                        <img className="rounded flex justify-center items-center   h-52 w-full" src={news.img} alt="" />
 
-                                <div className="mt-3 ">
-                                    <img className="rounded flex justify-center items-center   h-52 w-full" src={inter.img} alt="" />
 
-                                    <Link href={'/'} className=''>
-                                        <h2 className="border-b py-4 border-gray-300 ">{inter.heading?.slice(0, 80)}...</h2>
-                                    </Link>
-                                </div>
+                                        <h2 className="border-b py-4 border-gray-300 ">{news.heading?.slice(0, 80)}...</h2>
+                                    </div>
+                                </Link>
                             </div>)
                         }
                         <Button>
@@ -73,18 +73,18 @@ const InternationalNews = () => {
                     <div className="shadow-md p-3">
                         <h2><span className="font-semibold text-lg">{'Russia'}'s</span> top News</h2>
                         {
-                            russia.map(inter => <div className="my-3 duration-300 hover:-translate-y-2">
+                            russia.map(news => <div className="my-3 duration-300 hover:-translate-y-2">
+                                <Link href={`/news/view/${news?._id}`} className=''>
+                                    <div className="mt-3 ">
 
-                                <div className="mt-3 ">
+                                        <img className="rounded flex justify-center items-center   h-52 w-full" src={news.img} alt="" />
 
-                                    <img className="rounded flex justify-center items-center   h-52 w-full" src={inter.img} alt="" />
 
-                                    <Link href={'/'} className=''>
-                                        <h2 className="border-b py-4 border-gray-300 ">{inter.heading?.slice(0, 80)}...</h2>
+                                        <h2 className="border-b py-4 border-gray-300 ">{news.heading?.slice(0, 80)}...</h2>
 
-                                    </Link>
 
-                                </div>
+                                    </div>
+                                </Link>
                             </div>)
                         }
 
@@ -96,18 +96,18 @@ const InternationalNews = () => {
                     <div className="shadow-md p-3 ">
                         <h2 ><span className="font-semibold text-lg">{'Beijing'} 's</span> top News</h2>
                         {
-                            beijing.map(inter => <div className="my-3 duration-300 hover:-translate-y-2">
+                            beijing.map(news => <div className="my-3 duration-300 hover:-translate-y-2">
+                                <Link href={`/news/view/${news?._id}`} className=''>
+                                    <div className="mt-3 ">
 
-                                <div className="mt-3 ">
+                                        <img className="rounded flex justify-center items-center   h-52 w-full" src={news.img} alt="" />
 
-                                    <img className="rounded flex justify-center items-center   h-52 w-full" src={inter.img} alt="" />
 
-                                    <Link href={'/'} className=''>
-                                        <h2 className="border-b py-4 border-gray-300 ">{inter.heading?.slice(0, 80)}...</h2>
+                                        <h2 className="border-b py-4 border-gray-300 ">{news.heading?.slice(0, 80)}...</h2>
 
-                                    </Link>
 
-                                </div>
+                                    </div>
+                                </Link>
                             </div>)
                         }
 
@@ -119,16 +119,16 @@ const InternationalNews = () => {
                     <div className="shadow-md p-3">
                         <h2><span className="font-semibold text-lg">{'Japan'}'s</span> top News</h2>
                         {
-                            japan.map(inter => <div className="my-3 duration-300 hover:-translate-y-2">
+                            japan.map(news => <div className="my-3 duration-300 hover:-translate-y-2">
+                                <Link href={`/news/view/${news?._id}`} className=''>
+                                    <div className="mt-3 ">
 
-                                <div className="mt-3 ">
+                                        <img className="rounded flex justify-center items-center   h-52 w-full" src={news.img} alt="" />
 
-                                    <img className="rounded flex justify-center items-center   h-52 w-full" src={inter.img} alt="" />
-                                    <Link href={'/'} className=''>
-                                        <h2 className="border-b py-4 border-gray-300 ">{inter.heading?.slice(0, 80)}...</h2>
-                                    </Link>
+                                        <h2 className="border-b py-4 border-gray-300 ">{news.heading?.slice(0, 80)}...</h2>
 
-                                </div>
+                                    </div>
+                                </Link>
                             </div>
                             )
                         }
--- components/Home/TrendingNews/TrendingNews.js
+++ components/Home/TrendingNews/TrendingNews.js
@@ -18,51 +18,54 @@ const TrendingNews = () => {
         <div className="mt-28">
             <h1 className="font-bold text-2xl my-8">Trending News</h1>
             <div className="grid grid-cols-1 w-full  md:grid-cols-2 lg:grid-cols-4 gap-4">
-                <div className="  md:col-span-2 duration-300 hover:-translate-y-2 ">
+               
+                    <div className="  md:col-span-2 duration-300 hover:-translate-y-2 ">
+                    <Link href={`/news/view/${singleNews?._id}`}>
+                        <div className="flex min-h-[400px] h-full rounded items-end justify-center " style={{
+                            background: `linear-gradient(0deg, #151515eb 15%, rgba(6, 6, 6, 0) 100%) , url(${singleNews?.img})`,
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'cover',
+                        }}>
 
-                    <div className="flex min-h-[400px] h-full rounded items-end justify-center " style={{
-                        background: `linear-gradient(0deg, #151515eb 15%, rgba(6, 6, 6, 0) 100%) , url(${singleNews?.img})`,
-                        backgroundRepeat: 'no-repeat',
-                        backgroundSize: 'cover',
-                    }}>
-
-                        <div className=" text-white p-6  ">
-                            <h2 className="text-xl mb-2 ">{singleNews?.heading}</h2>
-                            <div className="flex text-sm ">
-                                <p className="pr-5 ">
-                                    By <span>{singleNews?.authorName}</span>
-                                </p>
-                                <p>{singleNews?.createdAt}</p>
+                           <div className=" text-white p-6  ">
+                                <h2 className="text-xl mb-2 ">{singleNews?.heading}</h2>
+                                <div className="flex text-sm ">
+                                    <p className="pr-5 ">
+                                        By <span>{singleNews?.authorName}</span>
+                                    </p>
+                                    <p>{singleNews?.createdAt}</p>
+                                </div>
                             </div>
+                           
                         </div>
+                        </Link>
                     </div>
-                </div>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 md:col-span-2 gap-4  ">
                     {/* 1 */}
 
                     {nextFourNews?.map((news) => (
-                        <div className="rounded shadow-md h-52 w-full duration-300 hover:-translate-y-2 " style={{
-                            background: `linear-gradient(0deg, #151515eb 15%, rgba(6, 10, 10, 0) 100%) , url(${news?.img})`,
-                            // backgroundImage: `url(${news?.img})`,
-                            backgroundRepeat: 'no-repeat',
-                            backgroundSize: 'cover',
-                            width: '100%'
-                        }}>
-                            <div className="flex items-end h-full  justify-center">
-                                <div className="p-4 text-white">
-                                    <Link className="text-white">
+                        <Link href={`/news/view/${news?._id}`}>
+                            <div className="rounded shadow-md h-52 w-full duration-300 hover:-translate-y-2 " style={{
+                                background: `linear-gradient(0deg, #151515eb 15%, rgba(6, 10, 10, 0) 100%) , url(${news?.img})`,
+                                // backgroundImage: `url(${news?.img})`,
+                                backgroundRepeat: 'no-repeat',
+                                backgroundSize: 'cover',
+                                width: '100%'
+                            }}>
+                                <div className="flex items-end h-full  justify-center">
+                                    <div className="p-4 text-white">
                                         <h2 className="text-sm mb-1">{singleNews?.heading}</h2>
-                                    </Link>
-                                    <div className="flex text-sm ">
-                                        <p className="pr-2">
-                                            By <span>{singleNews?.authorName}</span>
-                                        </p>
-                                        <p>{singleNews?.createdAt}</p>
+                                        <div className="flex text-sm ">
+                                            <p className="pr-2">
+                                                By <span>{singleNews?.authorName}</span>
+                                            </p>
+                                            <p>{singleNews?.createdAt}</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </Link>
                     ))}
                 </div>
             </div>
